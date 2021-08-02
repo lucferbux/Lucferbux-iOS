36dfadc056c860d7472a28fb8681d34a1ac5a3d5
Lucas Fernandez <lucasfernandezaragon@gmail.com>
2021-08-02 11:37:34 +0200

    Initial Commit

---
+++ Shared/lucferbux_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  lucferbux_iosApp.swift
+//  Shared
+//
+//  Created by lucas fernández on 2/8/21.
+//
+
+import SwiftUI
+
+@main
+struct lucferbux_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
